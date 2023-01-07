6cd69f54f8c88e5f2ce24dfb50f870d3ed1c55a9
LuizaLissandra <luizalissandrarosa@poli.ufrj.br>
2023-01-07 17:36:00 -0300

    update: Add access to different types of parameters

--- fundamentals-nodeJS/src/index.js
+++ fundamentals-nodeJS/src/index.js
@@ -4,13 +4,19 @@ const port = 3333;
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/courses", (request, response) => {
+    const query = request.query;
+    console.log(query);    
     return response.json(["Curso 1",
                           "Curso 2",
                           "Curso 3"])
 });
 
 app.post("/courses", (request, response) => {
+    const body = request.body;
+    console.log(body);
     return response.json(["Curso 1",
                           "Curso 2",
                           "Curso 3",
@@ -18,6 +24,8 @@ app.post("/courses", (request, response) => {
 });
 
 app.put("/courses/:id", (request, response) => {
+    const { id } = request.params;
+    console.log(id);
     return response.json(["Curso 0",
                           "Curso 1",
                           "Curso 2",
@@ -40,4 +48,3 @@ app.delete("/courses/:id", (request, response) => {
 app.listen(port, () => {
     console.log(`Running on port: ${port}.`)
 });
-
