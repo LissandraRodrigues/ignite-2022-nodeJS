e723c6cc042992d0052a4a8f5ab38910f544b9e1
LuizaLissandra <luizalissandrarosa@poli.ufrj.br>
2023-01-08 07:39:15 -0300

    feature: Add withdraw route

--- first-project/src/index.js
+++ first-project/src/index.js
@@ -27,6 +27,21 @@ function verifyIfExistsAccountCPF(request, response, next) {
 
 }
 
+// Middleware.
+function getBalance(statement) {
+
+    const balance = statement.reduce((accumulator, operation) => {
+        if(operation.type === 'credit') {
+            return accumulator + operation.amount;
+        } else {
+            return accumulator - operation.amount;
+        }
+    }, 0);
+
+    return balance;
+
+}
+
 app.post('/account', (request, response) => {
     const { name, cpf } = request.body;
   
@@ -56,6 +71,30 @@ app.post('/deposit', verifyIfExistsAccountCPF, (request, response) => {
         amount,
         created_at: new Date(),
         type: "credit"
+    };
+
+    customer.statement.push(statementOperation);
+
+    response.status(201).json({ message: "Operation performed successfully." });
+
+});
+
+app.post('/withdraw', verifyIfExistsAccountCPF, (request, response) => {
+
+    const { amount } = request.body;
+
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement);
+
+    if (balance < amount) {
+        return response.status(400).json({ error: "Insufficient fund." })
+    }
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: "debit"
     };
 
     customer.statement.push(statementOperation);
