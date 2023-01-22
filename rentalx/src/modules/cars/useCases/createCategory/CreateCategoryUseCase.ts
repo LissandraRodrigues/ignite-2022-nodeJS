3d1a37ed2bf50598b1ec0196e8dd5855dc02e08b
LuizaLissandra <luizalissandrarosa@poli.ufrj.br>
2023-01-22 20:11:59 -0300

    feature: Add useCase structure

--- rentalx/src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ rentalx/src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,11 +1,11 @@
-import { ICategoriesRepository } from "../repositories/ICategoriesRepository";
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
 interface IRequest {
     name: string;
     description: string;
 }
 
-class CreateCategoryService {
+class CreateCategoryUseCase {
 
     // Make the route that has to pass access to the repository.
     constructor(private categoriesRepository: ICategoriesRepository) { }
@@ -23,4 +23,4 @@ class CreateCategoryService {
     }
 }
 
-export { CreateCategoryService };
+export { CreateCategoryUseCase };
