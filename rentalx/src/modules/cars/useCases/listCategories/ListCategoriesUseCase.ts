6c7914b0b7a9a14615b69b755fa600c60c2ae069
LuizaLissandra <luizalissandrarosa@poli.ufrj.br>
2023-01-22 21:03:19 -0300

    feature: Add singleton pattern

---
+++ rentalx/src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
@@ -0,0 +1,16 @@
+import { Category } from "../../model/Category";
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+
+class ListCategoriesUseCase {
+
+    // Make the route that has to pass access to the repository.
+    constructor(private categoriesRepository: ICategoriesRepository) { }
+
+    execute(): Category[] {
+        const categories = this.categoriesRepository.list();
+        return categories;
+    }
+
+};
+
+export { ListCategoriesUseCase };
